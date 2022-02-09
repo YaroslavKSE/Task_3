a1798656e79041d902d504b8976a243c0ac5ff58
YaroslavKSE <ikhomych@kse.org.ua>
2022-02-09 12:29:27 +0200

    Intial commit

---
+++ Task-3/LinkedLIst.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Task_3
+{
+    public class KeyValuePair
+    {
+        public string Key { get; }
+
+        public string Value { get; }
+
+        public KeyValuePair(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+
+    public class LinkedListNode
+    {
+        public KeyValuePair Pair { get; }
+
+        public LinkedListNode Next { get; set; }
+
+        public LinkedListNode(KeyValuePair pair, LinkedListNode next = null)
+        {
+            Pair = pair;
+            Next = next;
+        }
+    }
+}
